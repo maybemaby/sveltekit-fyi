e035d1699381e74e884479a71876f4bb3784b509
maybemaby <74226064+maybemaby@users.noreply.github.com>
2026-06-15 16:11:37 -0700

    feat: responsive grid

--- web/src/routes/hits/+page.svelte
+++ web/src/routes/hits/+page.svelte
@@ -110,7 +110,7 @@
 		>
 	</div>
 
-	<div class="grid grid-cols-3 gap-2">
+	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
 		{#each datedDomains as domain (domain.domain)}
 			<Preview
 				domain={domain.domain}
