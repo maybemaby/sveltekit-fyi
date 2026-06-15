9e0c29836456e35b5fe6d5e20194408c8e4d3802
maybemaby <74226064+maybemaby@users.noreply.github.com>
2026-06-14 18:48:57 -0700

    refactor: add custom type for order

--- web/src/routes/scans.remote.ts
+++ web/src/routes/scans.remote.ts
@@ -2,21 +2,25 @@ import { query } from '$app/server';
 import { apiClient } from '$lib/api';
 import * as v from 'valibot';
 
-export const getDomains = query(
-	v.object({
-		page: v.optional(v.pipe(v.number(), v.integer())),
-		order: v.optional(v.picklist(['seen_at', 'seen_count']), 'seen_at')
-	}),
-	async (data) => {
-		const res = await apiClient.getDomainListings(data.page, data.order);
+const domainListingSchema = v.object({
+	page: v.optional(v.pipe(v.number(), v.integer())),
+	order: v.optional(v.picklist(['seen_at', 'seen_count']), 'seen_at')
+});
 
-		if (res.isErr()) {
-			throw res.error;
-		}
+type Defined<T> = T extends undefined ? never : T;
 
-		return res.value;
+export type DomainListingQuery = v.InferInput<typeof domainListingSchema>;
+export type DomainListingOrder = Defined<DomainListingQuery['order']>;
+
+export const getDomains = query(domainListingSchema, async (data) => {
+	const res = await apiClient.getDomainListings(data.page, data.order);
+
+	if (res.isErr()) {
+		throw res.error;
 	}
-);
+
+	return res.value;
+});
 
 export const getStats = query(async () => {
 	const res = await apiClient.getScanStats();
