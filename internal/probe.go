0b91f193cfc2cc3232c7793c1ccb76a81dec5ecb
maybemaby <74226064+maybemaby@users.noreply.github.com>
2026-06-26 19:35:33 -0700

    wip: svelte site probing

--- internal/probe.go
+++ internal/probe.go
@@ -1,15 +1,19 @@
 package internal
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"path"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+var svelteWindowRegex = regexp.MustCompile(`(?i)window\.__svelte\s*\?\?=\s*{}`)
 
 var querySelectors = []string{
 	"[data-sveltekit-preload-data]",
@@ -93,6 +97,71 @@ func probeOgImage(doc *goquery.Document) string {
 	}
 
 	return ""
+}
+
+func resolveRelativeUrl(baseUrl *url.URL, relativeUrl string) (*url.URL, error) {
+	parsedRelativeUrl, err := url.Parse(relativeUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	resolvedUrl := baseUrl.ResolveReference(parsedRelativeUrl)
+
+	return resolvedUrl, nil
+}
+
+func detectSvelte(src io.Reader) bool {
+	// Limit data read to 1.5MB to avoid reading too much data from large files
+	limitReader := io.LimitReader(src, 1024*1024*1.5)
+	bufReader := bufio.NewReader(limitReader)
+	// Odds of a 22 byte string being split across two reads is low, 0.004% for 5 KB
+	buffer := make([]byte, 1024*5)
+
+	for {
+		_, err := bufReader.Read(buffer)
+
+		isSvelte := svelteWindowRegex.Match(buffer)
+
+		if isSvelte {
+			return true
+		}
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return false
+		}
+	}
+
+	return false
+}
+
+func probeEntryUrls(doc *goquery.Document, pageUrl *url.URL) []string {
+	var urls []string
+
+	doc.Find("script").Each(func(i int, s *goquery.Selection) {
+		src, exists := s.Attr("src")
+
+		if exists && strings.HasSuffix(src, ".js") {
+			resolvedUrl, err := resolveRelativeUrl(pageUrl, src)
+			if err == nil && (resolvedUrl.Scheme == "http" || resolvedUrl.Scheme == "https") {
+				urls = append(urls, resolvedUrl.String())
+			}
+		}
+	})
+
+	doc.Find(`link[rel="modulepreload"]`).Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+
+		if exists && strings.HasSuffix(href, ".js") {
+			resolvedUrl, err := resolveRelativeUrl(pageUrl, href)
+			if err == nil && (resolvedUrl.Scheme == "http" || resolvedUrl.Scheme == "https") {
+				urls = append(urls, resolvedUrl.String())
+			}
+		}
+	})
+
+	return urls
 }
 
 func getImage(url string) (image []byte, err error) {
